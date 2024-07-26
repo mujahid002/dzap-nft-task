c44d51a661a674ff1c4320a4e7c344ee70ad5690
Mujahid002 <mujahidshaik2002@gmail.com>
2024-07-26 16:07:48 +0530

    added modifier

--- contracts/DZap-RewardToken.sol
+++ contracts/DZap-RewardToken.sol
@@ -1,42 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20, ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+error RewardToken__Unauthorized();
 
 /// @custom:security-contact mujahidshaik2002@gmail.com
-contract RewardToken is ERC20, ERC20Pausable, AccessControl {
-    bytes32 public constant DZAP_STAKING_NFT_CONTRACT = keccak256("DZAP_STAKING_NFT_CONTRACT");
+contract RewardToken is ERC20, ERC20Pausable {
+    address private immutable s_dZapStakingContractAddress;
 
-    constructor(address defaultAdmin, address dZapStakingNftContract)
-        ERC20("MyToken", "MTK")
+    constructor(address _dZapStakingNftContract)
+        ERC20("DZapRewardToken", "DZRT")
     {
-        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
-        _grantRole(DZAP_STAKING_NFT_CONTRACT, dZapStakingNftContract);
+        s_dZapStakingContractAddress = _dZapStakingNftContract;
     }
 
-    function pause() public onlyRole(DZAP_STAKING_NFT_CONTRACT) {
+     modifier onlyDZapStakingContract() {
+        if (_msgSender() != s_dZapStakingContractAddress)
+            revert RewardToken__Unauthorized();
+        _;
+    }
+
+    function pause() public onlyDZapStakingContract {
         _pause();
     }
 
-    function unpause() public onlyRole(DZAP_STAKING_NFT_CONTRACT) {
+    function unpause() public onlyDZapStakingContract {
         _unpause();
     }
 
-    function mintRewardToken(address to, uint256 amount) public onlyRole(DZAP_STAKING_NFT_CONTRACT) {
+    function mint(address to, uint256 amount)
+        public
+        onlyDZapStakingContract
+    {
         _mint(to, amount);
     }
-    function burnRewardToken(address from, uint256 amount) public onlyRole(DZAP_STAKING_NFT_CONTRACT) {
-        _burn(from,amount);
+
+    function burn(address from, uint256 amount)
+        public
+        onlyDZapStakingContract
+    {
+        _burn(from, amount);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Pausable)
-    {
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
         super._update(from, to, value);
     }
 }
