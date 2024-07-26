185c35ca7650e0bdd86c4d8ba9fce15302d60156
Mujahid002 <mujahidshaik2002@gmail.com>
2024-07-26 17:41:07 +0530

    updated with polygonAmoy testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,48 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@openzeppelin/hardhat-upgrades");
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 11155111,
+      timeout: 10000,
+      confirmations: 2,
+    },
+    polygonAmoy: {
+      url: process.env.AMOY_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 80002,
+      timeout: 10000,
+      confirmations: 2,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: process.env.AMOY_API_KEY,
+    },
+    customChains: [
+      {
+        network: "polygonAmoy",
+        chainId: 80002,
+        urls: {
+          apiURL: "https://api-amoy.polygonscan.com/api",
+          browserURL: "https://amoy.polygonscan.com",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: true,
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
 };
